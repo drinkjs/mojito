6bea0d7ad3992a378cfe47fd8799b3c53fbe7ecf
timou <284512180@qq.com>
2021-02-19 18:27:13 +0800

    删除多余文件

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,21 +1,16 @@
-// import * as dotenv from "dotenv";
-
-// 加载环境变量
-// dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
-
 export default {
   port: 3838,
   websocket: true, // 是否启用websocket
-  cluster: false, // 是否启用多进程
+  cluster: false, // 是否启用多进程，启用后websocket消息需要通过发布/订阅的方式处理
   logger: false,
   staticPrefix: "/public/",
-  staticPath: `${process.cwd()}/public`,
-  libPath: `${process.cwd()}/public/libs`,
+  staticPath: `${process.cwd()}/public`, // 所有静态文件存放访目录，用户上传的图片也存在这，生产环境建议放在cdn或nginx下
+  libPath: `${process.cwd()}/public/libs`, // 组件上传后存放的目录，生产环境建议放在cdn或nginx下
   mongo: {
     // see https://mongoosejs.com/
     uri: "mongodb://localhost:27017/",
     options: {
-      dbName: "test",
+      dbName: "mojito",
       useNewUrlParser: true,
       useUnifiedTopology: true,
       autoIndex: false,
@@ -31,7 +26,7 @@ export default {
   //   port: 3306,
   //   username: 'root',
   //   password: '',
-  //   database: 'screen_console',
+  //   database: 'test',
   //   entities: [
   //     `${__dirname}/../entity/*.ts`,
   //   ],
