b05ca204acaf7039458f6aa431e2cc9db416b148
Win102021DMMRWQ\Administrator <timoumail@gmai.com>
2023-08-09 12:12:30 +0800

    环境变量

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { visualizer } from "rollup-plugin-visualizer"
 import path from 'path';
@@ -6,30 +6,33 @@ import path from 'path';
 const PRO_HOST = "http://mojito.drinkjs.com"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), visualizer({ open: true })],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src') // @替代src
-    }
-  },
-  server: {
-    host:"0.0.0.0",
-    proxy: {
-      "/api": {
-        target: PRO_HOST,
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, '')
-      },
-      '/public': {
-        target: PRO_HOST,
-        changeOrigin: true
-      },
-      '/ws': {
-        target: PRO_HOST,
-        ws: true,
-        changeOrigin: true
+export default defineConfig(({ mode }) => {
+  const { VITE_HOST } = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react(), visualizer({ open: true })],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src') // @替代src
       }
     },
+    server: {
+      host: "0.0.0.0",
+      proxy: {
+        "/api": {
+          target: VITE_HOST,
+          changeOrigin: true,
+          // rewrite: (path) => path.replace(/^\/api/, '')
+        },
+        '/public': {
+          target: VITE_HOST,
+          changeOrigin: true
+        },
+        '/ws': {
+          target: VITE_HOST,
+          ws: true,
+          changeOrigin: true
+        }
+      },
+    }
   }
 })
