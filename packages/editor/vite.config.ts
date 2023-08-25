dfc78ad40572274741776bcaa13360878c91aee0
Win102021DMMRWQ\Administrator <timoumail@gmai.com>
2023-08-25 18:58:46 +0800

    removeConsole

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -2,12 +2,13 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 // import { visualizer } from "rollup-plugin-visualizer"
 import path from 'path';
+import removeConsole from "vite-plugin-remove-console";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const { VITE_HOST } = loadEnv(mode, process.cwd());
   return {
-    plugins: [react(), /* visualizer({ open: true }) */ ],
+    plugins: [react(), removeConsole(), /* visualizer({ open: true }) */ ],
     resolve: {
       alias: {
         '@': path.resolve('./src') // @替代src
